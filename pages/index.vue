4d548bed791b38729f2d2f1909b8810e66d7ec2a
khanhnguyenitq <nguyenkhanhnguyenk3@gmail.com>
2024-04-01 21:41:35 +0700

    post component

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,51 @@
 <template>
-    <div>
-        index
+  <MainLayout>
+    <div id="IndexPage" class="w-full overflow-auto">
+      <div class="mx-auto max-w-[500px] overflow-hidden">
+        <div id="Posts" class="px-4 max-w-[600px] mx-auto">
+          <div class="text-white" v-if="isPosts" v-for="post in posts" :key="post">
+            <Post :post="post" @isDeleted="posts = []" />
+          </div>
+          <div v-else>
+            <client-only>
+              <div v-if="isLoading" class="mt-20 w-full flex items-center justify-center mx-auto">
+                <div class="text-white mx-auto flex flex-col items-center justify-center">
+                  <Icon name="eos-icons:bubble-loading" size="50" color="#ffffff" />
+                  <div class="w-full mt-1">Loading...</div>
+                </div>
+              </div>
+              <div v-if="!isLoading" class="mt-20 w-full flex items-center justify-center mx-auto">
+                <div class="text-white mx-auto flex flex-col items-center justify-center">
+                  <Icon name="tabler:mood-empty" size="50" color="#ffffff" />
+                  <div class="w-full">Make the first post!</div>
+                </div>
+              </div>
+            </client-only>
+          </div>
+        </div>
+      </div>
     </div>
+  </MainLayout>
 </template>
 
 <script setup>
+import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/stores/user";
 
+const userStore = useUserStore();
+
+let posts = ref([]);
+let isPosts = ref(true);
+let isLoading = ref(false);
+
+onBeforeMount(() => {
+  posts.value = [
+    {
+      name: "nguyen",
+      image: "https://placehold.co/100",
+      text: "this is text",
+      picture: "https://placehold.co/500",
+    },
+  ];
+});
 </script>
